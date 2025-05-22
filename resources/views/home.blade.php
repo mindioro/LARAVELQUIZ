fea64372309657ffbd007f52ec02cebab68f2e52
lynx-psp <samuelprincepagtalunan@gmail.com>
2025-05-23 02:13:27 +0800

    update na pogi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">{{ __('Dashboard') }}</h1>
+                    <h1 class="m-0">{{ __('MarkPogi') }}</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -21,7 +21,7 @@
                     <div class="card">
                         <div class="card-body">
                             <p class="card-text">
-                                {{ __('You are logged in!') }}
+                                {{ __('You are logged in1!') }}
                             </p>
                         </div>
                     </div>
